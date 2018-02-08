1c81ade4af609f5fecb5dffa684b0afa657cf18b
calvin <butler2018@hotmail.com>
2018-02-08 14:20:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a18020803/MyJobService.java
@@ -0,0 +1,26 @@
+package com.example.student.a18020803;
+
+import android.app.Service;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyJobService extends JobService {
+    public MyJobService() {
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+
+        Log.d("JOB", "This is onStartJob");return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+
+
+}
